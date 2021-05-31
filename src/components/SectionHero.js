ea7d86d9cfb30835ed867cec92a2eede4340c25f
Stackbit Code Editor <projects@stackbit.com>
2021-05-31 18:16:22 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -21,8 +21,7 @@ export default class SectionHero extends React.Component {
                 </div>
                 )}
                 {_.get(section, 'actions', null) && (
-                <div className="block-buttons" style="
-    max-width: 640px;">
+                <div className="block-buttons" style="max-width: 640px;">
                   <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
                 </div>
                 )}
